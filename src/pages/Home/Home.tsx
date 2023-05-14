4c36d7df7c9c2155b8a560c0d37562c82bf1e7a1
Девятилов Максим <mdevyatilov@clink.tech>
2023-05-14 15:24:23 +0300

    commented unused code

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,14 @@
-import { useTheme, Typography } from '@mui/material';
-import styles from  './Home.module.scss';
+// import { useTheme } from '@mui/material';
+import styles from './Home.module.scss';
 
 const Home = () => {
-	const theme = useTheme();
+	// const theme = useTheme();
 	return (
 		// <Typography color={theme.palette.secondary.main} variant='h1'>
 		// 	home page
 		// </Typography>
 		<h1 className={styles.heading}>homepage</h1>
-	)
+	);
 };
 
 export { Home };
