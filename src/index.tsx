954651e31f236679e98a0e91111fb1a5186bcddc
Девятилов Максим <mdevyatilov@clink.tech>
2023-05-21 11:47:06 +0300

    #9 - Integrated Chakra UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,23 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import { ThemeProvider } from '@mui/material';
+import { ChakraProvider } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from 'router/router';
-
-// import { theme } from './theme/theme';
-
 import { store } from 'app/redux';
 
-// import '@fontsource/roboto/300.css';
-// import '@fontsource/roboto/400.css';
-// import '@fontsource/roboto/500.css';
-// import '@fontsource/roboto/700.css';
 import './index.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-	<React.StrictMode>
+	<StrictMode>
 		<Provider store={store}>
-			{/* <ThemeProvider theme={theme}> */}
-			<RouterProvider router={router} />
-			{/* </ThemeProvider> */}
+			<ChakraProvider>
+				<RouterProvider router={router} />
+			</ChakraProvider>
 		</Provider>
-	</React.StrictMode>
+	</StrictMode>
 );
