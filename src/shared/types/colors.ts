d9e1537e10f2a0db19715f98f4ec3107ca6478ba
Девятилов Максим <mdevyatilov@clink.tech>
2023-05-23 02:44:52 +0300

    #15 - Added ColorPalette

---
+++ src/shared/types/colors.ts
@@ -0,0 +1,11 @@
+export const enum ColorPalette {
+	BLACK_1 = 'rgba(21, 21, 21, 1)',
+	BLACK_2 = 'rgba(45, 55, 72, 1)',
+	GRAY_1 = 'rgba(204, 217, 224, 1)',
+	GRAY_2 = 'rgba(90, 110, 141, 1)',
+	GRAY_3 = 'rgba(237, 242, 247, 1)',
+	GRAY_4 = 'rgba(226, 232, 240, 1)',
+	PINK_1 = 'rgba(255, 75, 95, 1)',
+	BLUE_1 = 'rgba(0, 55, 145, 1)',
+	BLUE_2 = 'rgba(141, 167, 209, 1)',
+}
